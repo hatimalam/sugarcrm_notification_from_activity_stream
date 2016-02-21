21911ae94028fc9fdcaf2645319c7d154352fe07
Hatim Alam <hatimalam@gmail.com>
2016-02-21 12:58:58 +0530

    Update logic_hooks.php
    Corrected logic hook file name.

--- logic_hooks.php
+++ logic_hooks.php
@@ -7,7 +7,7 @@ $hook_array['before_save'] = array();
 $hook_array['before_save'][] = array(
 	1,
 	'Create Sugar Notification on any new post or comment',
-	'custom/modules/ActivityStream/Activities/customLogicHooks.php',
+	'custom/modules/ActivityStream/Activities/customLogicHook.php',
 	'SugarNotifyUser',
 	'notify_user_on_post_comment'
 );
