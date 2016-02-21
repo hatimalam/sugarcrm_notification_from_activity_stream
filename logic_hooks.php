e8a7433672806dbb6a4fd7bc97d178a20a0d5002
Hatim Alam <hatimalam@gmail.com>
2016-02-21 12:55:22 +0530

    Create before_save logic hook file
    Logic hook file to create Sugar notification on save of Activity Stream

---
+++ logic_hooks.php
@@ -0,0 +1,15 @@
+<?php
+
+$hook_version = 1;
+
+$hook_array = array();
+$hook_array['before_save'] = array();
+$hook_array['before_save'][] = array(
+	1,
+	'Create Sugar Notification on any new post or comment',
+	'custom/modules/ActivityStream/Activities/customLogicHooks.php',
+	'SugarNotifyUser',
+	'notify_user_on_post_comment'
+);
+
+?>
